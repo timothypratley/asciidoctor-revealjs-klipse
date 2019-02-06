f570ad93427b758d24c3b5872753bc0e7c000139
Timothy Pratley <timothypratley@gmail.com>
2019-02-05 17:53:13 -0800

    use CDN cached revealjs for easier building

--- bb.sh
+++ bb.sh
@@ -6,11 +6,5 @@ if [ ! -d .bundle ]; then
   bundle --path=.bundle/gems --binstubs=.bundle/.bin
 fi
 
-if [ ! -d site/reveal.js ]; then
-  cd site
-  git clone -b 3.6.0 --depth 1 https://github.com/hakimel/reveal.js.git
-  cd ..
-fi
-
 # build with dependencies
 bundle exec ./build.sh
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-FLAGS=--destination-dir=site
+FLAGS="--destination-dir=site -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0"
 
 echo "Building slides..."
 asciidoctor-revealjs ${FLAGS} docs/*.adoc

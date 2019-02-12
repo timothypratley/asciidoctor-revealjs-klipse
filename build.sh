114d966749ec1c77acb87c471ddd35cabda2a760
Timothy Pratley <timothypratley@gmail.com>
2019-02-11 17:51:56 -0800

    Tidy up slides

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-FLAGS="--destination-dir=site -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0"
+FLAGS="--destination-dir=site"
 
 echo "Building slides..."
 asciidoctor-revealjs ${FLAGS} docs/*.adoc
